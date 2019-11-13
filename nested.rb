9869880e718ca5a31f6e802fc0d9b589edd91094
dwane richards <dwanerich@gmail.com>
2019-11-13 00:18:01 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -106,8 +106,8 @@ def changing_alan
         }
      }
      
-     programmer_hash[:alan_kay][:known_for] = [alans_new_info => "GUI"]
-     p [alans_new_info]
+     p programmer_hash[:alan_kay][:known_for] = [alans_new_info => "GUI"]
+
      
      
      #change what Alan Kay is :known_for to the value of the #alans_new_info variable. 
