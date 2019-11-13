e647c8ce53aa4e5535acc0cf633127e5011cbe26
dwane richards <dwanerich@gmail.com>
2019-11-13 00:25:06 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -108,7 +108,6 @@ def changing_alan
      
      programmer_hash[:alan_kay][:known_for] = "Object Orientation"
      "Object Orientation" = "GUI"
-     p programmer_hash[:alan_kay][:known_for]
      
 
      
