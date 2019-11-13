da9aa78cef7a57fbfec30960f03eb4e9a92ae301
dwane richards <dwanerich@gmail.com>
2019-11-13 00:22:28 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -106,8 +106,7 @@ def changing_alan
         }
      }
      
-     programmer_hash[:alan_kay][:known_for] = [:alans_new_info]
-     p [:alans_new_info]
+     programmer_hash[:alan_kay][:known_for] = "GUI"
      
 
      
